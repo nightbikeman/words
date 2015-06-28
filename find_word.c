8d6a8f6c7b8f1f27f301db0d3fb6b5fcc718a5f6
Stephen Brooks <nightbikeman@gmail.com>
2015-06-28 21:19:14 +0100

    fixed word classification issue (I think)

--- find_word.c
+++ find_word.c
@@ -9,26 +9,25 @@ main (int argc, char *argv[])
 
     if (argc < 3)
     {
-        fprintf (stderr,
-                "Usage %s file word\n find word in file after loading into the hash\n",
-                argv[0]);
+        fprintf (stderr, "Usage %s file word\n find word in file after loading into the hash\n", argv[0]);
         exit (1);
     }
-	initialise(&w);
+    initialise (&w);
     WORDS_STAT ret = load (w, argv[1], TRUTHS);
-	if ( ret == WORDS_SUCCESS)
-	{
-    entity *e = find_word (w, argv[2]);
-
-    if (e)
+    ret = load (w, argv[1], UK_PLACE);
+    if (ret == WORDS_SUCCESS)
     {
-        printf ("found %s %s\n", e->name, word_type_str (e->type));
-        exit (0);
+        entity *e = find_word (w, argv[2]);
+
+        if (e)
+        {
+            printf ("found %s %s (%d)\n", e->name, word_type_str (e->type),e->type);
+            exit (0);
+        }
     }
-	}
-	else
-	{
-		fprintf(stderr,"Failed to load data");
-	}
+    else
+    {
+        fprintf (stderr, "Failed to load data");
+    }
     exit (1);
 }
